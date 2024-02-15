80507a62e74df3aa5936ac1dcbfd69bdf3a10b06
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2024-02-16 05:14:20 +0900

    feat: yana functionslar ustida ozgina ishladik

--- functions.js
+++ functions.js
@@ -5,3 +5,9 @@ function add(a,b){
 }
 const result = add(5,11)
 console.log("result:", result)
+
+function multiply(a,b){
+    return a * b
+}
+const natija = multiply(8,9)
+console.log("natija:", natija )
